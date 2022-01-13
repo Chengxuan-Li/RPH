11ea6ad60321f1bbb6ecc47e0b980946b72bbdbb
Chengxuan Li <lichengxuan@outlook.com>
2022-01-13 07:31:24 +0000

    Added printing to pdf code, but there is the issue of not printing linetype definitions.

--- RPHPrint.cs
+++ RPHPrint.cs
@@ -4,7 +4,9 @@ using Rhino.Geometry;
 using Rhino.Input;
 using Rhino.Input.Custom;
 using System;
+using System.IO;
 using System.Collections.Generic;
+
 
 namespace RPH
 {
@@ -37,17 +39,55 @@ namespace RPH
             if (full_path != null)
             {
                 String path = full_path.Split(path_sep, StringSplitOptions.None)[0];
-                String msg = String.Format("Current output directory is: {0}, do you wish a change?", path);
-
-
+                String msg = String.Format("Current output directory is: {0}, do you wish a change???", path);
 
                 //GetOption get_dir = new GetOption();
                 Boolean change_path = false;
                 RhinoGet.GetBool(msg, true, "NO", "YES", ref change_path);
 
+                if (change_path) {
+                    var opnDlg = new Rhino.UI.OpenFileDialog();//ANY dialog
+
+                        //opnDlg.Filter = "Png Files (*.png)|*.png";
+                        //opnDlg.Filter = "Excel Files (*.xls, *.xlsx)|*.xls;*.xlsx|CSV Files (*.csv)|*.csv"
+                        
+                    if (opnDlg.ShowOpenDialog());
+                    {
+                        //opnDlg.SelectedPath -- your result
+                    }
+                    
+                }
+
+                //printing
+
+                double original_linetypescale = doc.Linetypes.LinetypeScale;
+                String file_name = String.Concat(path, @"\20220113.pdf");
+                //String file_name = @"C:\Users\liche\Dropbox\PC\Desktop\EX12\Brief 04\Detail\Original\33.pdf";
+
+                Rhino.Display.RhinoPageView[] pages = doc.Views.GetPageViews();
+                Rhino.FileIO.FilePdf pdf = Rhino.FileIO.FilePdf.Create();
+                double dpi = 600;
+
+                // linetype issue!!!!!!!!!!
+
+                foreach (Rhino.Display.RhinoPageView page in pages)
+                {
+                    Rhino.DocObjects.DetailViewObject first_detail = page.GetDetailViews()[0];
+                    double page_scale = first_detail.Attributes.UserDictionary.GetDouble("page_scale");
+                    double linetypescale_multiplier = page_scale * 10;
+                    double print_linetypescale = linetypescale_multiplier * original_linetypescale;
+                    doc.Linetypes.LinetypeScale = print_linetypescale;
+
+                    Rhino.Display.ViewCaptureSettings captured_page = new Rhino.Display.ViewCaptureSettings(page, dpi);
+                    pdf.AddPage(captured_page);
+                }
+
+                pdf.Write(file_name);
+                doc.Linetypes.LinetypeScale = original_linetypescale;
+                
+
             }
 
-            var pages = doc.Views.GetPageViews();
 
             
             return Result.Success;
