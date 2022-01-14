5490f224a68efa7c1eccf9d48ba5597982958276
Chengxuan Li <lichengxuan@outlook.com>
2022-01-14 13:01:55 +0000

    RPHAddNewLayout Command

---
+++ RPHAddNewLayout.cs
@@ -0,0 +1,112 @@
+﻿using Rhino;
+using Rhino.Commands;
+using System;
+
+namespace RPH
+{
+    public class RPHAddNewLayout : Command
+    {
+        static RPHAddNewLayout _instance;
+        public RPHAddNewLayout()
+        {
+            _instance = this;
+        }
+
+        ///<summary>The only instance of the RPHAddNewLayout command.</summary>
+        public static RPHAddNewLayout Instance
+        {
+            get { return _instance; }
+        }
+
+        public override string EnglishName
+        {
+            get { return "RPHAddNewLayout"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+
+            Rhino.Display.RhinoPageView[] page_views = doc.Views.GetPageViews();
+            
+            // check whether all existing layouts are created with RPH
+
+
+            // return messages if not all layouts are created with RPH
+
+            // add new layout
+
+            // add new layout attributes
+
+            // add detail view
+
+            // adjust detail view position and scale
+
+            return Result.Success;
+        }
+    }
+}
+/*
+    double startX = 0.0;
+    ArchivableDictionary layout_info = new ArchivableDictionary();
+    RhinoDoc doc = RhinoDoc.ActiveDoc;
+    doc.PageUnitSystem = Rhino.UnitSystem.Millimeters;
+    var page_views = doc.Views.GetPageViews();
+    int page_number = (page_views == null) ? 1 : page_views.Length + 1;
+
+    if (_RunIt != true)
+    {
+      return;
+    }
+
+    layout_info.Set("page_number", page_number);
+
+    var pageview = doc.Views.AddPageView(string.Format("L{0}", page_number), extentX, extentY);
+    layout_info.Set("page_extent_X", extentX);
+    layout_info.Set("page_extent_Y", extentY);
+    layout_info.Set("detail_extent_X", drawingX);
+    layout_info.Set("detail_extent_Y", drawingY);
+    layout_info.Set("page_scale", scale);
+
+    for (int i = 1; i < page_number; i++)
+    {
+      Point3d origin = new Point3d(0, 0, 0);
+      Vector3d vector = new Vector3d(1, 0, 0);
+      Plane frame = new Plane(origin, vector);
+      var detail_viewport = page_views[i - 1].GetDetailViews()[0].Viewport;
+      detail_viewport.GetCameraFrame(out frame);
+      var box = detail_viewport.GetFrustumBoundingBox();
+      startX = box.GetCorners()[1].X;
+    }
+    layout_info.Set("page_bottom_left_X", startX);
+    if( pageview != null )
+    {
+      Rhino.Geometry.Point2d top_left = new Rhino.Geometry.Point2d(0, extentY);
+      Rhino.Geometry.Point2d bottom_right = new Rhino.Geometry.Point2d(extentX, 0);
+      var detail = pageview.AddDetailView("ModelView", top_left, bottom_right, Rhino.Display.DefinedViewportProjection.Top);
+
+      if (detail != null)
+      {
+        pageview.SetActiveDetail(detail.Id);
+        Rhino.Geometry.Point3d center = new Rhino.Geometry.Point3d(0.5 * drawingX + startX, 0.5 * drawingY, 0);
+        detail.Viewport.SetCameraLocation(center, true);
+        detail.Viewport.SetCameraTarget(center, true);
+        detail.CommitViewportChanges();
+        detail.DetailGeometry.IsProjectionLocked = true;
+        detail.DetailGeometry.SetScale(scale, doc.ModelUnitSystem, 1, doc.PageUnitSystem);
+        // Commit changes
+        // Append Rhino.ArchivableDictionary layout_info to Rhino.DocObjects.DetailViewObject.Attributes.UserDictionary
+        detail.Attributes.UserDictionary.AddContentsFrom(layout_info);
+        detail.CommitChanges();
+        Print(detail.Attributes.UserDictionary.ToString());
+
+      }
+      pageview.SetPageAsActive();
+      doc.Views.ActiveView = pageview;
+      doc.Views.Redraw();
+      NumSheet = page_number;
+      Size = drawingX;
+      return;
+    }
+    return;
+
+*/
